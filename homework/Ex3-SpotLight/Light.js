150a31a90a18dea0d6730ab2afa9758c23bc2492
LostBlackCat <wxy1999814@gmail.com>
2019-11-29 11:21:12 +0800

    wxy

--- homework/Ex3-SpotLight/Light.js
+++ homework/Ex3-SpotLight/Light.js
@@ -1,7 +1,10 @@
 var scene = null;
 var camera = null;
 var renderer = null;
-var mesh = null;
+var portalMesh = null;
+var hammerMesh = null;
+var youmuMesh = null;
+var grenadeMesh = null;
 var id = null;
 var myCanvas = null;
 
@@ -11,30 +14,78 @@ window.onload = function init() {
         canvas: document.getElementById('myCanvas')//画布
     });
     myCanvas = document.getElementById('myCanvas');
-    renderer.setClearColor(0x000000);//画布颜色
+    renderer.setClearColor(0x808080);//画布颜色
     scene = new THREE.Scene();//创建场景
     camera = new THREE.PerspectiveCamera(45,1,0.1,100);//透视投影照相机
     camera.position.set(0, 25, -10);//相机位置
     camera.lookAt(new THREE.Vector3(0, 5, 0));//lookAt()设置相机所看的位置
     scene.add(camera);//把相机添加到场景中
 
-    var OBJloader = new THREE.OBJLoader();
-    var MTLloader = new THREE.MTLloader();
-    MTLloader.load('thor hammer .mtl', function(material){
-        consle.log(material);
-        OBJloader.setMaterials(material);
-        OBJLoader.load('thor hammer .obj',function (obj){
-            obj.scale.set(0.05,0.05,0.05);
-            mesh = obj;
-            console.log(mesh);
+    var portalOBJLoader = new THREE.OBJLoader();
+    var portalMTLLoader = new THREE.MTLLoader();
+    portalMTLLoader.load('Portal Gun.mtl', function(material){
+        portalOBJLoader.setMaterials(material);
+        portalOBJLoader.load('Portal Gun.obj',function (obj){
+            obj.scale.set(1,1,1);
+            portalMesh = obj;
+            console.log(portalMesh);
             scene.add(obj);
         })
-    })
+    });
 
-    var light = new THREE.SpotLight(0xffffff);//光源颜色
+    var hammerOBJLoader = new THREE.OBJLoader();
+    var hammerMTLLoader = new THREE.MTLLoader();
+    hammerMTLLoader.load('Mjolnir.mtl', function(material){
+        hammerOBJLoader.setMaterials(material);
+        hammerOBJLoader.load('Mjolnir.obj',function (obj){
+            obj.position.set(5,0,0);
+            obj.scale.set(0.2,0.2,0.2);
+            hammerMesh = obj;
+            console.log(hammerMesh);
+            scene.add(obj);
+        })
+    });
+
+
+    var youmuOBJLoader = new THREE.OBJLoader();
+    var youmuMTLLoader = new THREE.MTLLoader();
+    youmuMTLLoader.load('katana.mtl', function(material){
+        youmuOBJLoader.setMaterials(material);
+        youmuOBJLoader.load('katana.obj',function (obj){
+            obj.position.set(10,0,0);
+            obj.scale.set(0.02,0.02,0.02);
+            youmuMesh = obj;
+            console.log(youmuMesh);
+            scene.add(obj);
+        })
+    });
+
+
+
+    var grenadeOBJLoader = new THREE.OBJLoader();
+    var grenadeMTLLoader = new THREE.MTLLoader();
+    grenadeMTLLoader.load('Grenade.mtl', function(material){
+        grenadeOBJLoader.setMaterials(material);
+        grenadeOBJLoader.load('Grenade.obj',function (obj){
+            obj.position.set(15,0,0);
+            obj.scale.set(0.1,0.1,0.1);
+            grenadeMesh = obj;
+            console.log(grenadeMesh);
+            scene.add(obj);
+        })
+    });
+
+
+
+
+
+    var light = new THREE.SpotLight(0xFFFFFF);//光源颜色
     light.position.set(20, 10, 5);//光源位置
     scene.add(light);//光源添加到场景中
     id = setInterval(draw,10);//每隔20s重绘一次
+
+    var control = new THREE.OrbitControls(camera,renderer.domElement);
+    control.update();
 };
 
 
